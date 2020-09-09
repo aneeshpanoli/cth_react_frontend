6f1d621f2bfb4dd4a1b9c7f353018a30b4456a41
aneesh <aneeshpanoli@gmail.com>
2020-09-08 20:54:41 -0700

    added fixed back to top

--- src/components/navigation/TopNav.jsx
+++ src/components/navigation/TopNav.jsx
@@ -26,6 +26,9 @@ import NoteAddIcon from "@material-ui/icons/NoteAdd";
 import { useHistory } from "react-router-dom";
 import { useTrackedState } from "reactive-react-redux";
 import { useTheme } from "@material-ui/core/styles";
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
 
 
 import PropTypes from 'prop-types';
@@ -59,7 +62,50 @@ HideOnScroll.propTypes = {
 };
 
 
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
 const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
   grow: {
     flexGrow: 1,
   },
@@ -232,6 +278,7 @@ export default function PrimarySearchAppBar(props) {
   };
 
   return (
+    <React.Fragment>
     <div className={classes.grow}>
       <CssBaseline />
       <HideOnScroll {...props}>
@@ -323,9 +370,18 @@ export default function PrimarySearchAppBar(props) {
           </div>
         </Toolbar>
       </AppBar>
+     
+
       </HideOnScroll>
+      <Toolbar id="back-to-top-anchor" />
       {renderMobileMenu}
       {renderMenu}
     </div>
+    <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
   );
 }
