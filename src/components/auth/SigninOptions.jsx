fe23c0e73cd4bbc272ee2c56fb69618504c7a357
Aneesh <aneeshpanoli@gmail.com>
2020-10-04 09:15:30 -0700

    fixed: social signin options not showing up

--- src/components/auth/SigninOptions.jsx
+++ src/components/auth/SigninOptions.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import EmailSignin from "./EmailSignin";
-// import SocialSignin from "./SocialSignin";
-
+import SocialSignin from "./SocialSignin";
 
 export default function SigninOptions(props) {
-  
   return (
     <Container component="main" maxWidth="xs" style={{ marginTop: "5%" }}>
-      <EmailSignin reset={props.reset} bttnStyle={props.bttnStyle}/>
-      {/*<SocialSignin signUp={props.signUp} bttnStyle={props.bttnStyle} />*/}
+      <EmailSignin reset={props.reset} bttnStyle={props.bttnStyle} />
+      <SocialSignin signUp={props.signUp} bttnStyle={props.bttnStyle} />
     </Container>
   );
 }
